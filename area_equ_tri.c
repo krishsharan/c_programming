fb0a6d7cc57ff7a3f2b439bd018ae842c6a53ced
krishsharan <krishsharan1998@gmail.com>
2019-06-18 12:23:02 +0530

    Create area_equ_tri.c

---
+++ area_equ_tri.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h> /math fuction
+void main() 
+{
+      int side,area;
+      printf("enter the side");
+      scanf("%d",&side);
+      area=((sqrt(3)/4)*side*side);
+      printf("area=%d",area);
+      
+      
+}
