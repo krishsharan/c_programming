31226d3003ff5131a0eed1702e0431d8dc2ccf08
krishsharan <krishsharan1998@gmail.com>
2019-06-18 16:52:14 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) 
+{
+	int num,rev=0,rem;
+	printf("Enter a num\n");
+	scanf("%d",&num);
+	while(num!=0)
+	{
+	 rem=num%10;
+	 rev=(rev*10)+rem;
+	 num/=10;
+	}
+	printf("\sum of digit:%d",rev);
+	return 0;
+}
