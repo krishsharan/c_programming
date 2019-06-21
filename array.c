bc293771e0e81e88afa5d18690ba85415f4a5717
krishsharan <krishsharan1998@gmail.com>
2019-06-21 14:14:59 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+    int arr[10],i;
+	for(i=0;i<10;i++)
+	{
+	 scanf("%d",&arr[i]);   
+	}
+	printf("Elements are:\n");
+	for(i=0;i<10;i++)
+	{
+	 printf("%d",arr[i]);
+	} 
+	return 0;
+}
+
